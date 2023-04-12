fff0d3c03c16802acb536829d5221d659002ebcc
Henry Zovaro <henry.zovaro@anu.edu.au>
2023-04-12 11:42:16 +1000

    Updated to use mar23 data

--- scripts/s7/plot_ages.py
+++ scripts/s7/plot_ages.py
@@ -17,101 +17,102 @@ from IPython.core.debugger import Tracer
 For each galaxy in our sample, plot the best-fit x_AGN and A_V.
 """
 
-s7_data_path = "/priv/meggs3/u5708159/S7/ppxf"
-fig_path = "/priv/meggs3/u5708159/S7/ppxf/figs/"
+s7_data_path = "/priv/meggs3/u5708159/S7/mar23/ppxf"
+fig_path = "/priv/meggs3/u5708159/S7/mar23/ppxf/figs/"
 
 ###############################################################################
 # Settings
 ###############################################################################
 savefigs = True
+aps = [ap.upper() for ap in sys.argv[1:]]
+for ap in aps:
+    assert ap in ["FOURAS", "ONEKPC", "RE1"], "ap must be one of 'FOURAS', 'ONEKPC', 'RE1'!"
+    
+    ###############################################################################
+    # Load the DataFrame
+    ###############################################################################
+    # Load the DataFrame containing all S7 galaxies
+    df_fname = f"s7_ppxf_{ap}.hd5"
+    df_all = pd.read_hdf(os.path.join(s7_data_path, df_fname), key="s7")
 
-###############################################################################
-# Load the DataFrame
-###############################################################################
-# Load the DataFrame containing all S7 galaxies
-df_fname = f"s7_ppxf.hd5"
-df_all = pd.read_hdf(os.path.join(s7_data_path, df_fname), key="s7")
+    gals = df_all.index.unique()
+    gals = gals.sort_values()
 
-if len(sys.argv) > 1:
-    gals = sys.argv[1:]
-else:
-    gals = df_all["Galaxy"].unique()
+    # Get the age & metallicity dimensions
+    _, _, metallicities, ages = load_ssp_templates("Padova")
 
-# Get the age & metallicity dimensions
-_, _, metallicities, ages = load_ssp_templates("Padova")
+    ###############################################################################
+    # Load the DataFrame from each galaxy & store the relevant values in a single 
+    # big DataFrame
+    ###############################################################################
+    df_ages = pd.DataFrame(index=gals)
+    df_ages.index.name = "Galaxy"
 
-###############################################################################
-# Load the DataFrame from each galaxy & store the relevant values in a single 
-# big DataFrame
-###############################################################################
-df_ages = pd.DataFrame(index=gals)
-df_ages.index.name = "Galaxy"
+    for gg, gal in enumerate(gals):
+        # Open the DataFrame
+        df_gal = df_all[df_all.index == gal]
 
-for gg, gal in enumerate(gals):
-    # Open the DataFrame
-    df_gal = df_all[df_all["Galaxy"] == gal]
+        # Extract the age index at the appropriate time 
+        for age in ages[1:]:
+            age_idx = np.nanargmin(np.abs(ages - age))
+            for col in ["MC mean", "MC error", "regularised"]:
+                df_ages.loc[gal, f"MW age (<{age/1e6:.2f} Myr) ({col})"] =\
+                    df_gal[f"Mass-weighted age vs. age cutoff ({col})"].values.item()[age_idx - 1]
+                df_ages.loc[gal, f"LW age (<{age/1e6:.2f} Myr) ({col})"] =\
+                    df_gal[f"Light-weighted age vs. age cutoff ({col})"].values.item()[age_idx - 1]
+                # Also compute the cumulative light fraction
+                df_ages.loc[gal, f"Cumulative light fraction (<{age/1e6:.2f} Myr) ({col})"] =\
+                    df_gal[f"Cumulative light fraction vs. age cutoff ({col})"].values.item()[age_idx - 1]
+                df_ages.loc[gal, "Number"] = gg
 
-    # Extract the age index at the appropriate time 
-    for age in ages[1:]:
-        age_idx = np.nanargmin(np.abs(ages - age))
-        for col in ["MC mean", "MC error", "regularised"]:
-            df_ages.loc[gal, f"MW age (<{age/1e6:.2f} Myr) ({col})"] =\
-                df_gal[f"Mass-weighted age vs. age cutoff ({col})"].values.item()[age_idx - 1]
-            df_ages.loc[gal, f"LW age (<{age/1e6:.2f} Myr) ({col})"] =\
-                df_gal[f"Light-weighted age vs. age cutoff ({col})"].values.item()[age_idx - 1]
-            # Also compute the cumulative light fraction
-            df_ages.loc[gal, f"Cumulative light fraction (<{age/1e6:.2f} Myr) ({col})"] =\
-                df_gal[f"Cumulative light fraction vs. age cutoff ({col})"].values.item()[age_idx - 1]
-            df_ages.loc[gal, "Number"] = gg
+    ###############################################################################
+    # Plot
+    ###############################################################################
+    for weighttype, marker in zip(["LW", "MW"], ["x", "D"]):
+        for age in [1e8, 1e9]:
+            fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(15, 5))
+            fig.subplots_adjust(bottom=0.3)
+            ax.set_title(f"{weighttype} weighted age " + r"($\tau_{\rm cutoff} = %.0f \,\rm Myr$)" % (age / 1e6))
 
-###############################################################################
-# Plot
-###############################################################################
-for weighttype, marker in zip(["LW", "MW"], ["x", "D"]):
-    for age in [1e8, 1e9]:
-        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(15, 5))
-        fig.subplots_adjust(bottom=0.3)
-        ax.set_title(f"{weighttype} weighted age " + r"($\tau_{\rm cutoff} = %.0f \,\rm Myr$)" % (age / 1e6))
+            #//////////////////////////////////////////////////////////////////////
+            # Axis 1: absolute values 
+            #//////////////////////////////////////////////////////////////////////
+            cond_good_regul = df_ages[f"Cumulative light fraction (<{age/1e6:.2f} Myr) (regularised)"] > 10**(-2.5)
+            cond_good_MC = df_ages[f"Cumulative light fraction (<{age/1e6:.2f} Myr) (MC mean)"] > 10**(-2.5)
+            
+            # Reliable
+            ax.errorbar(x=df_ages.loc[cond_good_regul, "Number"].values, 
+                        y=df_ages.loc[cond_good_regul, f"{weighttype} age (<{age/1e6:.2f} Myr) (regularised)"],
+                        marker=marker, mfc="lightgreen", mec="green", ecolor="green", linestyle="none", markersize=8,
+                        label="Regularised fit")
+            ax.errorbar(x=df_ages.loc[cond_good_MC, "Number"].values, 
+                        y=df_ages.loc[cond_good_MC, f"{weighttype} age (<{age/1e6:.2f} Myr) (MC mean)"],
+                        yerr=df_ages.loc[cond_good_MC, f"{weighttype} age (<{age/1e6:.2f} Myr) (MC error)"],
+                        marker=marker, mfc="red", mec="red", ecolor="red", alpha=0.5, linewidth=0.5, linestyle="none", markersize=8,
+                        label="MC simulations")
+            # Unreliable
+            ax.errorbar(x=df_ages.loc[~cond_good_regul, "Number"].values, 
+                        y=df_ages.loc[~cond_good_regul, f"{weighttype} age (<{age/1e6:.2f} Myr) (regularised)"],
+                        marker=marker, mfc="lightgrey", mec="grey", ecolor="grey", linestyle="none", markersize=8,
+                        label="Regularised fit (unreliable)")
+            ax.errorbar(x=df_ages.loc[~cond_good_MC, "Number"].values, 
+                        y=df_ages.loc[~cond_good_MC, f"{weighttype} age (<{age/1e6:.2f} Myr) (MC mean)"],
+                        yerr=df_ages.loc[~cond_good_MC, f"{weighttype} age (<{age/1e6:.2f} Myr) (MC error)"],
+                        marker=marker, mfc="lightgrey", mec="grey", ecolor="grey", alpha=0.5, linewidth=0.5, linestyle="none", markersize=8,
+                        label="MC simulations (unreliable)")
 
-        #//////////////////////////////////////////////////////////////////////
-        # Axis 1: absolute values 
-        #//////////////////////////////////////////////////////////////////////
-        cond_good_regul = df_ages[f"Cumulative light fraction (<{age/1e6:.2f} Myr) (regularised)"] > 10**(-2.5)
-        cond_good_MC = df_ages[f"Cumulative light fraction (<{age/1e6:.2f} Myr) (MC mean)"] > 10**(-2.5)
-        
-        # Reliable
-        ax.errorbar(x=df_ages.loc[cond_good_regul, "Number"].values, 
-                    y=df_ages.loc[cond_good_regul, f"{weighttype} age (<{age/1e6:.2f} Myr) (regularised)"],
-                    marker=marker, mfc="lightgreen", mec="green", ecolor="green", linestyle="none", markersize=8,
-                    label="Regularised fit")
-        ax.errorbar(x=df_ages.loc[cond_good_MC, "Number"].values, 
-                    y=df_ages.loc[cond_good_MC, f"{weighttype} age (<{age/1e6:.2f} Myr) (MC mean)"],
-                    yerr=df_ages.loc[cond_good_MC, f"{weighttype} age (<{age/1e6:.2f} Myr) (MC error)"],
-                    marker=marker, mfc="red", mec="red", ecolor="red", alpha=0.5, linewidth=0.5, linestyle="none", markersize=8,
-                    label="MC simulations")
-        # Unreliable
-        ax.errorbar(x=df_ages.loc[~cond_good_regul, "Number"].values, 
-                    y=df_ages.loc[~cond_good_regul, f"{weighttype} age (<{age/1e6:.2f} Myr) (regularised)"],
-                    marker=marker, mfc="lightgrey", mec="grey", ecolor="grey", linestyle="none", markersize=8,
-                    label="Regularised fit (unreliable)")
-        ax.errorbar(x=df_ages.loc[~cond_good_MC, "Number"].values, 
-                    y=df_ages.loc[~cond_good_MC, f"{weighttype} age (<{age/1e6:.2f} Myr) (MC mean)"],
-                    yerr=df_ages.loc[~cond_good_MC, f"{weighttype} age (<{age/1e6:.2f} Myr) (MC error)"],
-                    marker=marker, mfc="lightgrey", mec="grey", ecolor="grey", alpha=0.5, linewidth=0.5, linestyle="none", markersize=8,
-                    label="MC simulations (unreliable)")
+            # Decorations
+            ax.set_xticks(range(len(gals)))
+            ax.set_xticklabels(gals, rotation="vertical", fontsize="small")
+            ax.set_ylabel(f"{weighttype} weighted age (log Myr)")
+            ax.set_ylim([6, 10.5])
+            ax.grid()
+            ax.autoscale(axis="x", tight=True, enable=True)
+            ax.legend(fontsize="small")
+            ax.set_xlabel("Galaxy")
+            ax.set_xlim([-1, len(gals)])
 
-        # Decorations
-        ax.set_xticks(range(len(gals)))
-        ax.set_xticklabels(gals, rotation="vertical", fontsize="small")
-        ax.set_ylabel(f"{weighttype} weighted age (log Myr)")
-        ax.set_ylim([6, 10.5])
-        ax.grid()
-        ax.autoscale(axis="x", tight=True, enable=True)
-        ax.legend(fontsize="small")
-        ax.set_xlabel("Galaxy")
-        ax.set_xlim([-1, len(gals)])
-
-        if savefigs:
-            fig.savefig(os.path.join(fig_path, f"{weighttype}_ages_tau={age:.0e}.pdf"), bbox_inches="tight", format="pdf")
+            if savefigs:
+                fig.savefig(os.path.join(fig_path, f"{weighttype}_ages_tau={age:.0e}_{ap}.pdf"), bbox_inches="tight", format="pdf")
 
 
--- scripts/s7/plot_agn_params.py
+++ scripts/s7/plot_agn_params.py
@@ -17,104 +17,109 @@ from IPython.core.debugger import Tracer
 For each galaxy in our sample, plot the best-fit x_AGN and A_V.
 """
 
-s7_data_path = "/priv/meggs3/u5708159/S7/ppxf"
-fig_path = "/priv/meggs3/u5708159/S7/ppxf/figs/"
+s7_data_path = "/priv/meggs3/u5708159/S7/mar23/ppxf"
+fig_path = "/priv/meggs3/u5708159/S7/mar23/ppxf/figs/"
 
 ###############################################################################
 # Settings
 ###############################################################################
 savefigs = True
+aps = [ap.upper() for ap in sys.argv[1:]]
+for ap in aps:
+    assert ap in ["FOURAS", "ONEKPC", "RE1"], "ap must be one of 'FOURAS', 'ONEKPC', 'RE1'!"
 
-###############################################################################
-# Load the DataFrame
-###############################################################################
-# Load the DataFrame containing all S7 galaxies
-df_fname = f"s7_ppxf.hd5"
-df_all = pd.read_hdf(os.path.join(s7_data_path, df_fname), key="s7")
+    ###############################################################################
+    # Load the DataFrame
+    ###############################################################################
+    # Load the DataFrame containing all S7 galaxies
+    df_fname = f"s7_ppxf_{ap}.hd5"
+    df_all = pd.read_hdf(os.path.join(s7_data_path, df_fname), key="s7")
 
-if len(sys.argv) > 1:
-    gals = sys.argv[1:]
-else:
-    gals = df_all["Galaxy"].unique()
+    gals = df_all.index.unique()
+    gals = gals.sort_values()
 
-# Get the age & metallicity dimensions
-_, _, metallicities, ages = load_ssp_templates("Padova")
+    # Get the age & metallicity dimensions
+    _, _, metallicities, ages = load_ssp_templates("Padova")
 
-###############################################################################
-# Load the DataFrame from each galaxy & store the relevant values in a single 
-# big DataFrame
-###############################################################################
-df_ages = pd.DataFrame(index=gals)
-df_ages.index.name = "Galaxy"
+    ###############################################################################
+    # Load the DataFrame from each galaxy & store the relevant values in a single 
+    # big DataFrame
+    ###############################################################################
+    df_ages = pd.DataFrame(index=gals)
+    df_ages.index.name = "Galaxy"
 
-for gg, gal in enumerate(gals):
-    # Open the DataFrame
-    df_gal = df_all[df_all["Galaxy"] == gal]
-    df_ages.loc[gal, "x_AGN (total, regularised)"] = df_gal["x_AGN (total, regularised)"].values[0]
-    df_ages.loc[gal, "x_AGN (total, MC mean)"] = df_gal["x_AGN (total, MC mean)"].values[0]
-    df_ages.loc[gal, "x_AGN (total, MC error)"] = df_gal["x_AGN (total, MC error)"].values[0]
+    for gg, gal in enumerate(gals):
+        # Open the DataFrame
+        df_gal = df_all[df_all.index == gal]
+        df_ages.loc[gal, "x_AGN (total, regularised)"] = df_gal["x_AGN (total, regularised)"].values[0]
+        df_ages.loc[gal, "x_AGN (total, MC mean)"] = df_gal["x_AGN (total, MC mean)"].values[0]
+        df_ages.loc[gal, "x_AGN (total, MC error)"] = df_gal["x_AGN (total, MC error)"].values[0]
 
-    df_ages.loc[gal, "A_V (regularised)"] = df_gal["A_V (regularised)"].values[0]
-    df_ages.loc[gal, "A_V (MC mean)"] = df_gal["A_V (MC mean)"].values[0]
-    df_ages.loc[gal, "A_V (MC error)"] = df_gal["A_V (MC error)"].values[0]
-    df_ages.loc[gal, "Number"] = gg
+        df_ages.loc[gal, "A_V (regularised)"] = df_gal["A_V (regularised)"].values[0]
+        df_ages.loc[gal, "A_V (MC mean)"] = df_gal["A_V (MC mean)"].values[0]
+        df_ages.loc[gal, "A_V (MC error)"] = df_gal["A_V (MC error)"].values[0]
+        df_ages.loc[gal, "Number"] = gg
 
-###############################################################################
-# Plot x_AGN
-###############################################################################
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(15, 5))
-fig.subplots_adjust(bottom=0.3)
-ax.set_title(r"AGN continuum strength")
-ax.errorbar(x=df_ages["Number"].values, 
-            y=df_ages[f"x_AGN (total, regularised)"],
-            marker="o", mfc="lightgreen", mec="green", ecolor="green", linestyle="none", markersize=8,
-            label="Regularised fit")
-ax.errorbar(x=df_ages["Number"].values, 
-            y=df_ages[f"x_AGN (total, MC mean)"],
-            yerr=df_ages[f"x_AGN (total, MC error)"],
-            marker="o", mfc="red", mec="red", ecolor="red", alpha=0.5, linewidth=0.5, linestyle="none", markersize=8,
-            label="MC simulations")
+    ###############################################################################
+    # Plot x_AGN
+    ###############################################################################
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(15, 5))
+    fig.subplots_adjust(bottom=0.3)
+    ax.set_title(r"AGN continuum strength")
+    ax.errorbar(x=df_ages["Number"].values, 
+                y=df_ages[f"x_AGN (total, regularised)"],
+                marker="o", mfc="lightgreen", mec="green", ecolor="green", linestyle="none", markersize=8,
+                label="Regularised fit")
+    ax.errorbar(x=df_ages["Number"].values, 
+                y=df_ages[f"x_AGN (total, MC mean)"],
+                yerr=df_ages[f"x_AGN (total, MC error)"],
+                marker="o", mfc="red", mec="red", ecolor="red", alpha=0.5, linewidth=1.2, linestyle="none", markersize=8,
+                label="MC simulations")
 
-# Decorations
-ax.set_xticks(range(len(gals)))
-ax.set_xticklabels(gals, rotation="vertical", fontsize="small")
-ax.set_ylabel(r"$x_{\rm AGN}$")
-ax.grid()
-ax.autoscale(axis="x", tight=True, enable=True)
-ax.legend(fontsize="small")
-ax.set_xlabel("Galaxy")
-ax.set_xlim([-1, len(gals)])
+    # Decorations
+    ax.set_xticks(range(len(gals)))
+    ax.set_xticklabels(gals, rotation="vertical", fontsize="small")
+    ax.set_ylabel(r"$x_{\rm AGN}$")
+    ax.grid()
+    ax.autoscale(axis="x", tight=True, enable=True)
+    ax.legend(fontsize="small")
+    ax.set_xlabel("Galaxy")
+    ax.set_xlim([-1, len(gals)])
+    ax.set_ylim([-0.25, 4])
+    ax.axhline(0, color="k", ls="--")
 
-if savefigs:
-    fig.savefig(os.path.join(fig_path, f"x_AGN.pdf"), bbox_inches="tight", format="pdf")
+    if savefigs:
+        fig.savefig(os.path.join(fig_path, f"x_AGN_{ap}.pdf"), bbox_inches="tight", format="pdf")
 
-###############################################################################
-# Plot A_V
-###############################################################################
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(15, 5))
-fig.subplots_adjust(bottom=0.3)
-ax.set_title(r"Extinction")
-ax.errorbar(x=df_ages["Number"].values, 
-            y=df_ages[f"A_V (regularised)"],
-            marker="o", mfc="lightgreen", mec="green", ecolor="green", linestyle="none", markersize=8,
-            label="Regularised fit")
-ax.errorbar(x=df_ages["Number"].values, 
-            y=df_ages[f"A_V (MC mean)"],
-            yerr=df_ages[f"A_V (MC error)"],
-            marker="o", mfc="red", mec="red", ecolor="red", alpha=0.5, linewidth=0.5, linestyle="none", markersize=8,
-            label="MC simulations")
+    ###############################################################################
+    # Plot A_V
+    ###############################################################################
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(15, 5))
+    fig.subplots_adjust(bottom=0.3)
+    ax.set_title(r"Extinction")
+    ax.errorbar(x=df_ages["Number"].values, 
+                y=df_ages[f"A_V (regularised)"],
+                marker="o", mfc="lightgreen", mec="green", ecolor="green", linestyle="none", markersize=8,
+                label="Regularised fit")
+    ax.errorbar(x=df_ages["Number"].values, 
+                y=df_ages[f"A_V (MC mean)"],
+                yerr=df_ages[f"A_V (MC error)"],
+                marker="o", mfc="red", mec="red", ecolor="red", alpha=0.5, linewidth=1.2, linestyle="none", markersize=8,
+                label="MC simulations")
 
-# Decorations
-ax.set_xticks(range(len(gals)))
-ax.set_xticklabels(gals, rotation="vertical", fontsize="small")
-ax.set_ylabel(r"$A_V$ (mag)")
-ax.grid()
-ax.autoscale(axis="x", tight=True, enable=True)
-ax.legend(fontsize="small")
-ax.set_xlabel("Galaxy")
-ax.set_xlim([-1, len(gals)])
+    # Decorations
+    ax.set_xticks(range(len(gals)))
+    ax.set_xticklabels(gals, rotation="vertical", fontsize="small")
+    ax.set_ylabel(r"$A_V$ (mag)")
+    ax.grid()
+    ax.autoscale(axis="x", tight=True, enable=True)
+    ax.legend(fontsize="small")
+    ax.set_xlabel("Galaxy")
+    ax.set_xlim([-1, len(gals)])
+    ax.set_ylim([-0.25, 5])
+    ax.axhline(0, color="k", ls="--")
 
-if savefigs:
-    fig.savefig(os.path.join(fig_path, f"A_V.pdf"), bbox_inches="tight", format="pdf")
+    if savefigs:
+        fig.savefig(os.path.join(fig_path, f"A_V_{ap}.pdf"), bbox_inches="tight", format="pdf")
 
 
