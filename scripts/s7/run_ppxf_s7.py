9280b159b67c728d1a55b2a4002c3b8d3a12d670
Henry Zovaro <henry.zovaro@anu.edu.au>
2023-04-18 11:11:35 +1000

    Added bad_pixel_ranges_A to input args of run_ppxf() for MC runs

--- scripts/s7/run_ppxf_s7.py
+++ scripts/s7/run_ppxf_s7.py
@@ -12,36 +12,52 @@ import multiprocessing
 from ppxftests.run_ppxf import run_ppxf, add_stuff_to_df
 
 import matplotlib.pyplot as plt
-plt.ion()
+plt.ion()   
 plt.close("all")
 
 from IPython.core.debugger import Tracer 
 
 # Paths
-lzifu_input_path = "/priv/meggs3/u5708159/S7/LZIFU/data/"
-lzifu_output_path = "/priv/meggs3/u5708159/S7/LZIFU/products/"
-s7_data_path = "/priv/meggs3/u5708159/S7/"
+lzifu_input_path = "/priv/meggs3/u5708159/S7/mar23/LZIFU/data/"
+lzifu_output_path = "/priv/meggs3/u5708159/S7/mar23/LZIFU/products/"
+s7_data_path = "/priv/meggs3/u5708159/S7/mar23/"
+
+# Aperture type
+from enum import Enum
+class Aperture(Enum):
+    RE1 = 0
+    FOURAS = 1
+    SDSS = 2
+    ONEKPC = 3
 
 ###########################################################################
 # User options
 ###########################################################################
-debug = False  # If true, run on only 20 threads & save to a new DataFrame with 'DEBUG' in the file name
+args = sys.argv
+if args[1].upper() == "DEBUG":
+    debug = True  # If true, run on only 20 threads & save to a new DataFrame with 'DEBUG' in the file name
+    args = args[1:]  
+else:
+    debug = False
 nthreads = 20 if debug else 56
 niters = 20 if debug else 1000
 
+# x and y coordinates in DataCube corresponding to the chosen aperture
+aperture = Aperture[args[1]]
+rr, cc = np.unravel_index(aperture.value, (2, 2))
+
 # List of Galaxies
-if len(sys.argv) > 1:
-    gals = sys.argv[1:]
+if len(args) > 2:
+    gals = args[2:]
 else:
-    df_s7 = pd.read_csv(os.path.join(s7_data_path, "s7_study_sample_classifications.csv"))
-    gals = df_s7["S7 Galaxy Name"].values
+    gals = [g.strip("\n") for g in open(os.path.join(s7_data_path, "gal_list.txt")).readlines()]
 
 ###########################################################################
 # Helper function for running MC simulations
 ###########################################################################
 def ppxf_helper(args):
     # Unpack arguments
-    seed, spec, spec_err, lambda_vals_rest_A = args
+    seed, spec, spec_err, lambda_vals_rest_A, bad_pixel_ranges_A = args
     
     # Add "extra" noise to the spectrum
     rng = RandomState(seed)
@@ -61,6 +77,7 @@ def ppxf_helper(args):
     # Run ppxf
     pp = run_ppxf(spec=spec_noise, spec_err=spec_err, lambda_vals_A=lambda_vals_rest_A,
                   isochrones="Padova",
+                  bad_pixel_ranges=bad_pixel_ranges_A,
                   z=0.0, ngascomponents=1,
                   fit_gas=False, tie_balmer=False,
                   fit_agn_cont=True,
@@ -73,18 +90,22 @@ def ppxf_helper(args):
 ##############################################################################
 for gal in gals:
     # Create a new DataFrame for this galaxy
-    df_fname = f"s7_ppxf_DEBUG_{gal}.hd5" if debug else f"s7_ppxf_{gal}.hd5"
+    df_fname = f"s7_ppxf_DEBUG_{gal}_{aperture.name}.hd5" if debug else f"s7_ppxf_{gal}_{aperture.name}.hd5"
     df = pd.DataFrame(dtype="object")
     print("###################################################################")
-    print(f"Now running on {gal}...")
+    if not debug:
+        print(f"Now running on {gal} (aperture {aperture})...")
+    else:
+        print(f"Now running on {gal} (aperture {aperture}) in DEBUG mode...")
     print("###################################################################")
     ##############################################################################
     # Load the aperture spectrum
     ##############################################################################
     hdulist_in = fits.open(os.path.join(lzifu_input_path, f"{gal}.fits.gz"))
-    spec = hdulist_in[0].data[:, 0, 0]
-    spec_err = np.sqrt(hdulist_in[1].data[:, 0, 0])
-    norm = hdulist_in[0].header["NORM"]
+    
+    spec = hdulist_in["PRIMARY"].data[:, rr, cc]
+    spec_err = np.sqrt(hdulist_in["VARIANCE"].data[:, rr, cc])
+    norm = hdulist_in["NORM"].data[rr, cc]
     z = hdulist_in[0].header["Z"]
 
     N_lambda = hdulist_in[0].header["NAXIS3"]
@@ -108,8 +129,8 @@ for gal in gals:
     ##############################################################################
     lzifu_fname = f"{gal}_merge_comp.fits"
     hdulist = fits.open(os.path.join(lzifu_output_path, lzifu_fname))
-    spec_cont_lzifu = hdulist["CONTINUUM"].data[:, 0, 0] 
-    spec_elines = hdulist["LINE"].data[:, 0, 0]
+    spec_cont_lzifu = hdulist["CONTINUUM"].data[:, rr, cc] 
+    spec_elines = hdulist["LINE"].data[:, rr, cc]
 
     spec_cont_only = spec - spec_elines
 
@@ -148,7 +169,7 @@ for gal in gals:
     print("###################################################################")
     # Input arguments
     seeds = list(np.random.randint(low=0, high=100 * niters, size=niters))
-    args_list = [[s, spec_cont_only * norm, spec_err * norm, lambda_vals_rest_A] for s in seeds]
+    args_list = [[s, spec_cont_only * norm, spec_err * norm, lambda_vals_rest_A, bad_pixel_ranges_A] for s in seeds]
 
     # Run in parallel
     print(f"Gal {gal}: MC simulations: running ppxf on {nthreads} threads...")
@@ -166,7 +187,7 @@ for gal in gals:
     df = df.append(thisrow, ignore_index=True)
 
     # Save 
-    df.to_hdf(os.path.join(s7_data_path, "ppxf", df_fname), key="s7")
+    df.to_hdf(os.path.join(s7_data_path, "ppxf", df_fname), key=aperture.name)
     print("###################################################################")
     print(f"Finished processing {gal}!")
     print("###################################################################")
