194660db68cd156f89d97cb8e343e87d303f7859
Henry Zovaro <henry.zovaro@anu.edu.au>
2023-09-26 11:53:19 +1000

    Now saves age DataFrame plus stellar kinematics - want to replace save_ages_dataframe.py and consolidate_s7_dfs_kinematics.py

--- scripts/s7/consolidate_s7_dfs.py
+++ scripts/s7/consolidate_s7_dfs.py
@@ -1,35 +1,103 @@
 import os 
+import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
-"""
-In run_ppxf_s7.py, we saved the results from each galaxy to their own DataFrame.
-However, in a previous versino of the script, we saved 21 galaxies ino a single 
-DataFrame, called s7_ppxf.hd5. 
-Here, we append the results from those galaxies with individual .hd5 files
-to s7_ppxf.hd5, so that all galaxies can be stored in a single file.
-"""
+# Aperture type
+from enum import Enum
+class Aperture(Enum):
+    RE1 = 0
+    FOURAS = 1
+    SDSS = 2
+    ONEKPC = 3
 
-s7_data_path = "/priv/meggs3/u5708159/S7/ppxf"
+from gal_list import gals
 
-# Open the DataFrame that will contain ALL galaxies
-df_all = pd.read_hdf(os.path.join(s7_data_path, "s7_ppxf.hd5"))
+from IPython.core.debugger import Tracer
 
-# One by one, open the DataFrames containing results for individual galaxies
-df_fnames = [f for f in os.listdir(s7_data_path) 
-             if f.endswith(".hd5")
-             and f.startswith("s7_ppxf_")
-             and "DEBUG" not in f]
+# from ppxftests.mockspec import FWHM_WIFES_INST_A # TODO fix broken import
+FWHM_WIFES_INST_A = 1.4
+from ppxftests.ssputils import log_rebin_and_convolve_stellar_templates
 
-for df_fname in df_fnames:
-    # Open the dataFrame
-    df_gal = pd.read_hdf(os.path.join(s7_data_path, df_fname))
-    
-    # Get the name of the galaxy
-    gal = df_gal["Galaxy"].unique()[0]
+s7_data_path = "/priv/meggs3/u5708159/S7/mar23/ppxf"
 
-    # Only add this DataFrame if the galaxy is not already present.
-    if gal not in df_all["Galaxy"].unique():
-        df_all = df_all.append(df_gal, ignore_index=True)
+debug = False
+overwrite_dataframe = True
 
-# Save back to file
-df_all.to_hdf(os.path.join(s7_data_path, "s7_ppxf.hd5"), key="s7")
+# Load the stellar ages to put in the DataFrame
+_, _, metallicities, ages =\
+    log_rebin_and_convolve_stellar_templates(isochrones="Padova", metals_to_use=['004', '008', '019'], 
+                                                FWHM_inst_A=FWHM_WIFES_INST_A, 
+                                                velscale=1.0) # NOTE: velscale here is a dummy parameter since we just want the ages, not the templates
+
+for ap in [Aperture.FOURAS, Aperture.ONEKPC, Aperture.RE1]:
+
+    # Open the DataFrame that will contain ALL galaxies
+    df_fname = f"s7_ppxf_{ap.name}.hd5"
+    df_all = pd.DataFrame()
+
+    # One by one, open the DataFrames containing results for individual galaxies
+    for gg, gal in tqdm(enumerate(gals)):
+
+        # Filenames
+        df_ppxf_fname = f"s7_ppxf_{gal}_{ap.name}.hd5" 
+        df_stekin_fname = f"s7_ppxf_kinematics_{gal}_{ap.name}.hd5" 
+
+        # Open the dataFrames
+        df_ppxf = pd.read_hdf(os.path.join(s7_data_path, df_ppxf_fname)).set_index("Galaxy")
+        df_stekin = pd.read_hdf(os.path.join(s7_data_path, df_stekin_fname)).set_index("Galaxy")
+        df_thisgal = df_ppxf.merge(df_stekin[[c for c in df_stekin if c not in df_ppxf]], left_index=True, right_index=True)
+
+        # Compute LW/MW ages at all cutoff ages  
+        for age in ages[1:]:
+            age_idx = np.nanargmin(np.abs(ages - age))
+            for col in ["MC mean", "MC error", "MC 50th percentile", "MC 16th percentile", "MC 84th percentile", "regularised"]:
+                df_thisgal.loc[gal, f"MW age (<{age/1e6:.2f} Myr) ({col})"] =\
+                    df_ppxf.loc[gal, f"Mass-weighted age vs. age cutoff ({col})"][age_idx - 1]
+                df_thisgal.loc[gal, f"LW age (<{age/1e6:.2f} Myr) ({col})"] =\
+                    df_ppxf.loc[gal, f"Light-weighted age vs. age cutoff ({col})"][age_idx - 1]
+                # Also compute the cumulative light fraction
+                df_thisgal.loc[gal, f"Cumulative light fraction (<{age/1e6:.2f} Myr) ({col})"] =\
+                    df_ppxf.loc[gal, f"Cumulative light fraction vs. age cutoff ({col})"][age_idx - 1]
+
+        # Append to main DataFrame
+        df_all = df_all.append(df_thisgal)
+
+    # Sort
+    df_all = df_all.sort_index()
+    df_all["Number"] = np.arange(len(gals))
+
+    # Remove MC stellar kinematics so that they aren't accidentally used instead of the measurements from the VdS fit 
+    df_all = df_all.drop(columns=[c for c in df_all if ("sigma_*" in c or "v_*" in c) and "VdS" not in c])
+
+    # Save back to file
+    if not overwrite_dataframe and os.path.exists(os.path.join(s7_data_path, df_fname)):
+        k = input(f"File {os.path.join(s7_data_path, df_fname)} exists - are you sure you want to overwrite it? ")
+        if k.lower().startswith("y"):
+            df_all.to_hdf(os.path.join(s7_data_path, df_fname), key="s7")
+        else:
+            print("WARNING: not overwriting existing file!")
+    else:
+        df_all.to_hdf(os.path.join(s7_data_path, df_fname), key="s7")
+
+    # Only save AGN contributions
+    cols = ["x_AGN (total, regularised)", "x_AGN (total, MC mean)", "x_AGN (total, MC error)", "x_AGN (total, MC 16th percentile)", "x_AGN (total, MC 50th percentile)", "x_AGN (total, MC 84th percentile)",]
+    df_agn = df_all[cols].copy()
+    df_agn.to_csv(os.path.join(s7_data_path, f"s7_ppxf_{ap.name}_xAGN.csv"))
+
+    # Only save A_V values
+    cols = ["A_V (regularised)", "A_V (MC mean)", "A_V (MC error)", "A_V (MC 16th percentile)", "A_V (MC 50th percentile)", "A_V (MC 84th percentile)",]
+    df_av = df_all[cols].copy()
+    df_av.to_csv(os.path.join(s7_data_path, f"s7_ppxf_{ap.name}_A_V.csv"))
+
+    # Only save ages
+    cols = [c for c in df_all if ("1000.0" in c or "100.0" in c) and "age" in c]
+    cols += [c for c in df_all if "17783.0" in c and "age" in c]
+    df_ages = df_all[cols].copy()
+    df_ages = df_ages.rename(columns=dict(zip(cols, [c.replace(" (<17783.00 Myr)", "") + " (log yr)" for c in cols])))
+    df_ages.to_csv(os.path.join(s7_data_path, f"s7_ppxf_{ap.name}_ages.csv"))
+
+    # Only save kinematics
+    cols = [c for c in df_all if "VdS" in c]
+    df_stekin = df_all[cols].copy()
+    df_stekin.to_csv(os.path.join(s7_data_path, f"s7_ppxf_{ap.name}_kinematics.csv"))
