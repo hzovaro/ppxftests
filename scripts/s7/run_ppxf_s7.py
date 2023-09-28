84baaf55b08eb3fd5bc0eec79e2473bf2cee6ca4
Henry Zovaro <henry.zovaro@anu.edu.au>
2023-09-28 12:48:41 +1000

    Implemented load_galaxy_spectrum

--- scripts/s7/run_ppxf_s7.py
+++ scripts/s7/run_ppxf_s7.py
@@ -10,7 +10,8 @@ from astropy.io import fits
 from tqdm import tqdm
 from time import time
 
-from settings import CLEAN, lzifu_input_path, lzifu_output_path, ppxf_output_path, fig_path, Aperture, gals_all
+from settings import CLEAN, ppxf_output_path, fig_path, Aperture, get_aperture_coords, gals_all
+from load_galaxy_spectrum import load_galaxy_spectrum
 from ppxftests.run_ppxf import run_ppxf, add_stuff_to_df
 
 import matplotlib.pyplot as plt
@@ -33,7 +34,7 @@ niters = 10 if debug else 1000
 
 # x and y coordinates in DataCube corresponding to the chosen aperture
 aperture = Aperture[args[1]]
-rr, cc = np.unravel_index(aperture.value, (2, 2))
+rr, cc = get_aperture_coords(aperture)
 
 # List of Galaxies
 if len(args) > 2:
@@ -77,7 +78,6 @@ def ppxf_helper(args):
 
 ##############################################################################
 # Run ppxf for each galaxy & append to DataFrame
-##############################################################################
 for gal in gals:
     # Create a new DataFrame for this galaxy
     df_fname = f"s7_ppxf_DEBUG_{gal}_{aperture.name}.hd5" if debug else f"s7_ppxf_{gal}_{aperture.name}.hd5"
@@ -86,57 +86,14 @@ for gal in gals:
         print(f"Now running on {gal} (aperture {aperture})...")
     else:
         print(f"Now running on {gal} (aperture {aperture}) in DEBUG mode...")
+
     ##############################################################################
     # Load the aperture spectrum
-    ##############################################################################
-    hdulist_in = fits.open(os.path.join(lzifu_input_path, f"{gal}.fits.gz"))
-    
-    spec = hdulist_in["PRIMARY"].data[:, rr, cc]
-    spec_err = np.sqrt(hdulist_in["VARIANCE"].data[:, rr, cc])
-    norm = hdulist_in["NORM"].data[rr, cc]
-    z = hdulist_in[0].header["Z"]
-
-    N_lambda = hdulist_in[0].header["NAXIS3"]
-    dlambda_A = hdulist_in[0].header["CDELT3"]
-    lambda_0_A = hdulist_in[0].header["CRVAL3"]
-    lambda_vals_obs_A = np.array(list(range(N_lambda))) * dlambda_A + lambda_0_A
-    lambda_vals_rest_A = lambda_vals_obs_A / (1 + z)
-
-    # Define bad pixel ranges
-    bad_pixel_ranges_A = [
-         [(6300 - 10) / (1 + z), (6300 + 10) / (1 + z)], # Sky line at 6300
-         [(5577 - 10) / (1 + z), (5577 + 10) / (1 + z)], # Sky line at 5577
-         [(6360 - 10) / (1 + z), (6360 + 10) / (1 + z)], # Sky line at 6360
-         [(5700 - 10) / (1 + z), (5700 + 10) / (1 + z)], # Sky line at 5700                              
-         [(5889 - 30), (5889 + 20)], # Interstellar Na D + He line 
-         [(5889 - 10) / (1 + z), (5889 + 10) / (1 + z)], # solar NaD line
-    ]
-
-    ##############################################################################
-    # Load the LZIFU fit
-    ##############################################################################
-    lzifu_fname = f"{gal}_merge_comp.fits"
-    hdulist = fits.open(os.path.join(lzifu_output_path, lzifu_fname))
-    spec_cont_lzifu = hdulist["CONTINUUM"].data[:, rr, cc] 
-    spec_elines = hdulist["LINE"].data[:, rr, cc]
-
-    spec_cont_only = spec - spec_elines
-
-    # Plot to check
-    if debug:
-        fig, ax = plt.subplots(figsize=(10, 5))
-        ax.errorbar(x=lambda_vals_obs_A, y=spec, yerr=spec_err, color="k", label="Data")
-        ax.plot(lambda_vals_obs_A, spec_cont_lzifu, color="green", label="Continuum fit")
-        ax.plot(lambda_vals_obs_A, spec_elines, color="magenta", label="Emission line fit")
-        ax.plot(lambda_vals_obs_A, spec_cont_lzifu + spec_elines, color="orange", label="Total fit")
-        ax.plot(lambda_vals_obs_A, spec_cont_only, color="red", label="Data minus emission lines")
-        ax.set_xlabel("Wavelength (Å)")
-        ax.set_ylabel(r"Normalised flux ($F_\lambda$)")
-        ax.legend()
+    lambda_vals_obs_A, lambda_vals_rest_A, spec, spec_cont_only, spec_err, norm, bad_pixel_ranges_A =\
+        load_galaxy_spectrum(gal, aperture)
 
     ##############################################################################
     # PPXF: MC simulations
-    ##############################################################################
     # Input arguments
     seeds = list(np.random.randint(low=0, high=100 * niters, size=niters))
     args_list = [[s, spec_cont_only * norm, spec_err * norm, lambda_vals_rest_A, bad_pixel_ranges_A] for s in seeds]
@@ -150,12 +107,9 @@ for gal in gals:
 
     ##############################################################################
     # Extract information for DataFrame
-    ##############################################################################
+    plot_fname = plot_fname=f"MC_iter_{gal}_{aperture.name}_debug.pdf" if debug else plot_fname=f"MC_iter_{gal}_{aperture.name}.pdf"
     try:
-        if debug:
-            thisrow = add_stuff_to_df(pp_mc_list, plotit=True, fig_path=fig_path, plot_fname=f"MC_iter_{gal}_{aperture.name}_debug.pdf", savefig=True, gal=gal)
-        else:
-            thisrow = add_stuff_to_df(pp_mc_list, plotit=True, fig_path=fig_path, plot_fname=f"MC_iter_{gal}_{aperture.name}.pdf", savefig=True, gal=gal)
+        thisrow = add_stuff_to_df(pp_mc_list, plotit=True, fig_path=fig_path, plot_fname=plot_fname, savefig=True, gal=gal)
     except:
         print(f"WARNING: plotting failed for {gal}!")
         thisrow = add_stuff_to_df(pp_mc_list)
