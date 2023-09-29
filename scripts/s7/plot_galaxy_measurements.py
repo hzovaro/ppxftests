acbdc3b79240cf628b20cf7857ede6a9a055a6a8
Henry Zovaro <henry.zovaro@anu.edu.au>
2023-09-29 12:21:15 +1000

    No longer plots galaxies in gals_unreliable_stellar_measurements; changed figsize

--- scripts/s7/plot_galaxy_measurements.py
+++ scripts/s7/plot_galaxy_measurements.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 
-from settings import ppxf_output_path, fig_path, Aperture
+from settings import ppxf_output_path, fig_path, Aperture, gals_unreliable_stellar_measurements
 
 import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
@@ -11,9 +11,7 @@ plt.ion()
 plt.close("all")
 
 """
-For each galaxy in our sample, plot the LW or MW ages, plus the best-fit x_AGN and A_V.
-In the same figure, plot (a) ages, (b) A_V and (c) x_AGN. 
-Only show x-axis labels on the top/bottom w/ rotation.
+For each galaxy in our sample, plot (a) ages, (b) A_V and (c) x_AGN. 
 """
 
 ###############################################################################
@@ -37,7 +35,7 @@ colour_dict = {
 ###############################################################################
 weighttype = "LW"
 
-fig, axs = plt.subplots(nrows=3, ncols=1, figsize=(15, 15))
+fig, axs = plt.subplots(nrows=3, ncols=1, figsize=(15, 10))
 ax_AGN, ax_AV, ax_ages = axs
 fig.subplots_adjust(hspace=0)
 
@@ -50,14 +48,13 @@ for ap in [Aperture.FOURAS, Aperture.ONEKPC, Aperture.RE1]:
     ###############################################################################
     # Load the DataFrame containing ages 
     df = pd.read_hdf(os.path.join(ppxf_output_path, f"s7_ppxf_{ap.name}.hd5"), key="S7")
-    gals = df.index.values
+    gals_to_plot = [g for g in df.index.values if g not in gals_unreliable_stellar_measurements]
 
     ###############################################################################
     # Plot ages
     ###############################################################################
     for age_str in age_cutoff_strs:
         age = float(age_str)
-        cond_good_regul = df[f"Cumulative light fraction (<{age/1e6:.2f} Myr) (regularised)"] > 10**(-2.5)
         cond_good_MC = df[f"Cumulative light fraction (<{age/1e6:.2f} Myr) (MC mean)"] > 10**(-2.5)
         
         #//////////////////////////////////////////////////////////////////////
@@ -105,8 +102,8 @@ for ap in [Aperture.FOURAS, Aperture.ONEKPC, Aperture.RE1]:
                 marker="s", mfc=colour_dict[ap.name], mec=colour_dict[ap.name], ecolor=colour_dict[ap.name], alpha=0.5, linewidth=1.2, linestyle="none", markersize=4,)
 
 # Decorations
-ax_ages.set_xticklabels(gals, rotation="vertical", ha="center", va="top", fontsize="small")
-ax_AGN.set_xticklabels(gals, rotation="vertical", ha="center", va="bottom", fontsize="small")
+ax_ages.set_xticklabels(gals_to_plot, rotation="vertical", ha="center", va="top", fontsize="small")
+ax_AGN.set_xticklabels(gals_to_plot, rotation="vertical", ha="center", va="bottom", fontsize="small")
 ax_ages.tick_params(top=False, labeltop=False, bottom=True, labelbottom=True)
 ax_AGN.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False)
 ax_ages.set_ylim([6, 10.5])
@@ -115,13 +112,13 @@ ax_ages.set_ylabel(f"{'Light' if weighttype == 'LW' else 'Mass'}-weighted age (l
 ax_AV.set_ylabel(r"$A_V$ (mag)")
 ax_AGN.set_ylabel(r"$x_{\rm AGN}$")
 for ax in axs:
-    ax.set_xticks(range(len(gals)))
-    ax.set_xticks(np.array(range(len(gals))) + 0.5, minor=True)
+    ax.set_xticks(range(len(gals_to_plot)))
+    ax.set_xticks(np.array(range(len(gals_to_plot))) + 0.5, minor=True)
     ax.grid(b=True, which="minor", axis="x")
     ax.grid(b=True, which="major", axis="y")
     ax.autoscale(axis="x", tight=True, enable=True)
     ax.set_xlabel("Galaxy")
-    ax.set_xlim([-1, len(gals)])
+    ax.set_xlim([-1, len(gals_to_plot)])
 
 # Legend
 legend_elements = [
